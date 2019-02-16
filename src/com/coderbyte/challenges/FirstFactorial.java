d8b29c5a0f0e86a34e2491557c3642aab6de5ecc
Nitin96Bisht <nitin.bisht96@gmail.com>
2019-02-16 21:50:04 +0530

    First Reverse

--- src/com/coderbyte/challenges/FirstFactorial.java
+++ src/com/coderbyte/challenges/FirstFactorial.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /**
  * Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. 
  * For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. 
- * For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
+ * For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+ * @author Nitin Bisht 
  */
 public class FirstFactorial {
 
---
+++ src/com/coderbyte/challenges/FirstReverse.java
@@ -0,0 +1,20 @@
+package com.coderbyte.challenges;
+
+/**
+ * Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+ * For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH. 
+ * @author Nitin Bisht
+ *
+ */
+public class FirstReverse {
+
+	public static void main(String[] args) {
+
+		System.out.println(FirstReverse("I Love Code"));
+	}
+
+	private static String FirstReverse(String str) {
+		return new StringBuilder(str).reverse().toString();
+	}
+
+}
