ad716a8d03b4d9947fa4a60e331a9debb4a8878b
Nitin96Bisht <nitin.bisht96@gmail.com>
2019-02-17 00:13:27 +0530

    Simple Adding

--- src/com/coderbyte/challenges/SimpleAdding.java
+++ src/com/coderbyte/challenges/SimpleAdding.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /**
  * Have the function SimpleAdding(num) add up all the numbers from 1 to num. 
  * For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be any number from 1 to 1000. 
+ * 
  * @author Nitin Bisht
- *
  */
 public class SimpleAdding {
 
